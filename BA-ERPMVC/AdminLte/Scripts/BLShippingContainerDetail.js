70d22c2410f9a28af704edc70b316377b53b919b
Bilal Associates <Bilal Associates@DESKTOP-LJ9TAOV>
2022-04-23 16:40:05 +0500

    js

---
+++ BA-ERPMVC/AdminLte/Scripts/BLShippingContainerDetail.js
@@ -0,0 +1,82 @@
+﻿$(document).ready(function myfunction() {
+
+    $(document).on("click", ".btnSaveEdit", function () {
+        row = $(this).closest("tr")
+        console.log(row.find(".txt_ID").val())
+        save(row, false)
+
+    })
+
+    $(document).on("click", ".btndlt", function () {
+        var row = $(this).closest("tr")
+        save(row, true)
+    })
+
+    $('#example').dataTable({
+        "paging": false
+    });
+
+    function save(row, IsDeleted) {
+
+        if (row.find(".txt_ID").val() == 0 && IsDeleted == true) {
+            alert('You can not delete to non existing record !!')
+            return false;
+        }
+       var containersList =  row.find(".txt_ContainerNo").parent().html()
+        var dataObject = JSON.stringify({
+            'Id': row.find(".txt_ID").val(),
+            'IsDeleted': IsDeleted,
+            'Bl': row.find(".txtblNo").val(),
+            'ContainerNo': row.find(".txt_ContainerNo").val(),
+            'SealNo': row.find(".txt_sealno").val(),
+
+        });
+        showLoader();
+        fetch('/BLShippingLine/BlShippingContainerDetail', {
+            method: 'POST',
+            body: dataObject,
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+        }).then(res => res.json()).then(function (response) {
+            if (response.success) {
+                if (IsDeleted) {
+                    row.remove();
+                }
+                else {
+                    if (row.find(".txt_ID").val() == 0) {
+                        $('#example').prepend(`<tr class="bg-light tbl-valign-top">
+                             <td>
+                                <input type="hidden" value="0" class="form-control txt_ID" />
+                                <input type="text" disabled style="width: 200px;" value="`+ row.find(".txtblNo").val() +`" class="form-control" />
+
+
+                            </td>
+                            <td>
+                                `+ containersList+`
+                            </td>
+                            <td>
+
+                                <input oninput="this.value= this.value.toUpperCase()" UPO type="text" style="width: 200px;" value="" class="form-control  txt_sealno" />
+
+                            </td>
+
+                            <td class="btn-group">
+                                <button type="button" class="btn btn-sm btn-block btn-success btn-v2 fs-8 text-nowrap mt-1 mb-0 btnSaveEdit">Save/Update</button>&ensp;
+                                <button type="button" class="btn btn-sm btn-block btn-danger btn-v2 fs-8 text-nowrap mt-1 mb-0 btndlt">Delete</button>
+                            </td>
+                        </tr>`)
+                    }
+
+                    row.find(".txt_ID").val(response.Id)
+
+                }
+
+
+                hideLoader();
+            }
+            else showErrorMessage(response.message);
+        });
+    }
+
+})
